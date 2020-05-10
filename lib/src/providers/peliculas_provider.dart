f4cb9b60d59cdb9524912a3d49d128fb22087116
halley rios <halley@lacafetalab.pe>
2020-05-09 23:42:35 -0500

    initial projet fluter

---
+++ lib/src/providers/peliculas_provider.dart
@@ -0,0 +1,39 @@
+
+import 'package:peliculas/src/models/pelicula.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class PeliculasProvider {
+  String _apiKey    = '6366296bfcd6ca019da1d1988199ab10';
+  String _url       = 'api.themoviedb.org';
+  String _language  = 'es-ES';
+
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async{
+     final resp = await http.get(url);
+     final decodedData = json.decode(resp.body);
+     final peliculas = new Peliculas.fromJsonList(decodedData['results']);
+     return peliculas.items;
+  }
+
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, '3/movie/now_playing',{
+      'api_key'  : _apiKey,
+      'language': _language
+    });
+
+    return this._procesarRespuesta(url);
+
+  }
+
+  Future<List<Pelicula>> getPopulares() async {
+    final url = Uri.https(_url,'3/movie/popular', {
+      'api_key'  : _apiKey,
+      'language': _language
+    });
+    return this._procesarRespuesta(url);
+
+   
+    
+  }
+  
+}
